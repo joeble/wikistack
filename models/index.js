d0111be2ce500d807ad3ec88ff99f5b6373410de
SangWoo Kim <sangwoo1111@gmail.com>
2017-08-07 08:05:43 -0700

    sangwoo3

--- models/index.js
+++ models/index.js
@@ -1,18 +1,46 @@
-var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/wikistack');
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack', { logging: false });
 
 const Page = db.define('page', {
-  title: Sequelize.STRING,
-  url: Sequelize.STRING,
-  content: Sequelize.TEXT,
-  status: {
-    type: Sequelize.ENUM('open', 'closed')
-  }
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    urlTitle: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed')
+    },
+    date: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
+    }
+}, {
+      getterMethods: {
+        route() {
+          return '/wiki/' + this.urlTitle;
+        }
+      }
 });
 
 const User = db.define('user', {
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    }
 });
 
 module.exports = {
